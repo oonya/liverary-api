b09bb7ec426833867f28f1ca321a41efe4dad326
oonya <oonya0816@icloud.com>
2021-03-15 01:33:31 +0900

    ダミーを返す

--- app.py
+++ app.py
@@ -8,12 +8,43 @@ app = Flask(__name__)
 CORS(app, support_credentials=True)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+app.config['JSON_AS_ASCII'] = False
+
 
 @app.route('/')
 def deltee_config():
     return jsonify({"res" : "response"})
 
 
+@app.route('/words')
+def show_words():
+    uuid = "uuid"
+    with open('responses/words.json', mode='r', buffering=-1, encoding='utf-8') as f:
+        res = json.loads(f.read())
+
+    return jsonify(res)
+
+
+
+@app.route('/word_num_list')
+def get_word_num_list():
+    uuid = "uuid"
+    with open('responses/word_num_list.json', mode='r', buffering=-1, encoding='utf-8') as f:
+        res = json.loads(f.read())
+
+    return jsonify(res)
+
+
+@app.route('/delete', methods=['POST'])
+def delete_word():
+    f = request.get_data()
+    form_data = json.loads(f.decode('utf-8'))
+    word = form_data['word']
+    uuid = "uuid"
+
+    print(word, '\n', uuid, '\n\n')
+
+    return '', 204
 
 
 if __name__ == '__main__':
