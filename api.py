f9406a52416e1bf26ef36d2fe2eb2a3b2264dd44
oonya <oonya0816@icloud.com>
2021-04-20 20:11:36 +0900

    word_num_listの処理を切り出す

--- api.py
+++ api.py
@@ -28,3 +28,26 @@ class Api():
 
         return jsonify(res)
     
+
+    def word_num_list(uuid):
+        with open('responses/word_num_list.json', mode='r', buffering=-1, encoding='utf-8') as f:
+            res = json.loads(f.read())
+
+        a = db_session.query(Words).filter(Words.uuid==uuid).all()
+
+        dic_ary = []
+        for m in a:
+            dic_ary = Api.inc_res(dic_ary, m)
+        
+        res['word_num_list'] = sorted(dic_ary, key=lambda x:x['month'])
+
+        return jsonify(res)
+
+    def inc_res(dic_array, word):
+        for d in dic_array:
+            if d["month"] == word.date[:7]:
+                d["sum"] += 1
+                return dic_array
+        
+        dic_array.append({"month":word.date[:7], "sum":1})
+        return dic_array
--- app.py
+++ app.py
@@ -41,9 +41,7 @@ def show_words():
     except Exception:
         return 'Unauthorized?', 401
 
-
     return Api.show_words(uuid)
-
 
 
 @app.route('/word_num_list')
@@ -56,29 +54,7 @@ def get_word_num_list():
     except Exception:
         return 'Unauthorized?', 401
 
-
-    with open('responses/word_num_list.json', mode='r', buffering=-1, encoding='utf-8') as f:
-        res = json.loads(f.read())
-
-    a = db_session.query(Words).filter(Words.uuid==uuid).all()
-
-    dic_ary = []
-    for m in a:
-        dic_ary = inc_res(dic_ary, m)
-    
-    res['word_num_list'] = sorted(dic_ary, key=lambda x:x['month'])
-
-    return jsonify(res)
-
-
-def inc_res(dic_array, word):
-    for d in dic_array:
-        if d["month"] == word.date[:7]:
-            d["sum"] += 1
-            return dic_array
-    
-    dic_array.append({"month":word.date[:7], "sum":1})
-    return dic_array
+    return Api.word_num_list(uuid)
 
 
 @app.route('/delete', methods=['POST'])
